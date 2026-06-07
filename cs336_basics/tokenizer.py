7f6d96e986553068520c9c1724034cdb988ce4f7
Bowen Cheng <bowencheng2027@u.northwestern.edu>
2026-06-07 13:10:01 -0500

    pass tokenizer test cases!

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -2,17 +2,21 @@ import json
 import regex as re
 
 from collections.abc import Iterable, Iterator
-from collections import defaultdict
 
 
 class Tokenizer:
   def __init__(self, vocab: dict[int, bytes], 
                merges: list[tuple[bytes, bytes]], 
-               special_tokens: list[int] | None = None):
+               special_tokens: list[str] | None = None):
     # tokenID -> bytes
     self.vocab = vocab
     self.merges = merges
-    self.special_tokens = special_tokens
+    
+    if special_tokens:
+      # When encoding, group by larger special tokens first
+      self.special_tokens = sorted(special_tokens, key=len, reverse=True)
+    else:
+      self.special_tokens = None
     
     # bytes -> tokenID
     self.reverse_vocab = {
@@ -24,7 +28,7 @@ class Tokenizer:
   def from_files(cls,
                  vocab_filepath: str,
                  merges_filepath: str,
-                 special_tokens: list[int] | None = None):
+                 special_tokens: list[str] | None = None):
     with open(vocab_filepath, "r", encoding="utf-8") as f:
       raw_vocab = json.load(f)
   
@@ -51,22 +55,20 @@ class Tokenizer:
     PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
     
     if self.special_tokens:
-      special_tokens_str = []
-      
-      for special_token in self.special_tokens:
-        special_tokens_str.append(self.vocab[special_token].decode("utf-8"))
-      
-      
-      special_pattern = "|".join([re.escape(special_token_str) for special_token_str in special_tokens_str])
-      chunks = re.split(special_pattern, text)
+      special_pattern = "|".join([re.escape(special_token) for special_token in self.special_tokens])
+      text_and_special_tokens = re.split(f"({special_pattern})", text) # capture group to keep the special tokens
     else:
-      chunks = [text]
+      text_and_special_tokens = [text]
       
-    for chunk in chunks:
-      matches = re.finditer(PAT, chunk)
+    for text_or_special_token in text_and_special_tokens:
+      if self.special_tokens and text_or_special_token in self.special_tokens:
+        pretokens.append((text_or_special_token.encode("utf-8"), )) # acting as special_token
       
-      for match in matches:
-        pretokens.append(tuple(bytes([b]) for b in match.group(0).encode("utf-8")))
+      else: 
+        matches = re.finditer(PAT, text_or_special_token) # acting as text
+        
+        for match in matches:
+          pretokens.append(tuple(bytes([b]) for b in match.group(0).encode("utf-8")))
         
     return pretokens
   
@@ -77,28 +79,42 @@ class Tokenizer:
     token_id_seq = []
     
     for pretoken in pretokens:
-      n = len(pretoken)
-      new_pretoken = [] 
+      if self.special_tokens and pretoken[0].decode("utf-8") in self.special_tokens:
+        token_id_seq.append(self.reverse_vocab[pretoken[0]])
+        continue
+      
+      cur_pretoken = pretoken
       
       for merge in self.merges:
+        n = len(cur_pretoken)
+        new_pretoken = [] 
+        
         new_symbol = merge[0] + merge[1]
         
         i = 0
         while i < n:
-          if i + 1 < n and pretoken[i] == merge[0] and pretoken[i+1] == merge[1]:
+          if i + 1 < n and cur_pretoken[i] == merge[0] and cur_pretoken[i+1] == merge[1]:
             new_pretoken.append(new_symbol)
             i += 2
           else:
-            new_pretoken.append(pretoken[i])
+            new_pretoken.append(cur_pretoken[i])
             i += 1
             
-      for token in new_pretoken:
+        cur_pretoken = tuple(new_pretoken)
+            
+      for token in cur_pretoken:
         token_id_seq.append(self.reverse_vocab[token])
         
     return token_id_seq
   
   def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
-    raise NotImplementedError
+    for text in iterable:
+      yield from self.encode(text)
   
   def decode(self, ids: list[int]) -> str:
-    raise NotImplementedError
+    byte_seq = b""
+    
+    for id in ids:
+      byte_seq += self.vocab[id]
+      
+    return byte_seq.decode("utf-8", errors="replace")
--- tests/adapters.py
+++ tests/adapters.py
@@ -9,7 +9,7 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
-from cs336_basics import train_bpe
+from cs336_basics import train_bpe, tokenizer
 
 
 def run_linear(
@@ -561,7 +561,7 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    return tokenizer.Tokenizer(vocab, merges, special_tokens)
 
 
 def run_train_bpe(
