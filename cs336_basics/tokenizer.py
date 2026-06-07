e0f2ff3388c0ec7c022e0f1765df688ba018b3dd
Bowen Cheng <bowencheng2027@u.northwestern.edu>
2026-06-07 17:04:47 -0500

    stream input text instead of loading it all in ram

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -89,10 +89,7 @@ class Tokenizer:
     
     token_id_seq = []
     
-    for pretoken in tqdm(pretokens,
-                         desc="Encoding",
-                         unit="pretoken",
-                         total=len(pretokens)):
+    for pretoken in pretokens:
       if (self.special_tokens 
           and self.special_token_bytes
           and len(pretoken) == 1
@@ -143,15 +140,88 @@ class Tokenizer:
   def decode(self, ids: list[int]) -> str:
     byte_seq = b"".join(self.vocab[id] for id in ids)
     return byte_seq.decode("utf-8", errors="replace")
+  
+
+def find_chunk_boundaries(
+  file: BinaryIO,
+  chunk_size: int,
+  split_special_token: bytes,
+) -> list[int]:
+  """
+  Chunk the file into parts that can be counted independently.
+  May return fewer chunks if the boundaries end up overlapping.
+  """
+  assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+  # Get total file size in bytes
+  file.seek(0, os.SEEK_END)
+  file_size = file.tell()
+  file.seek(0)
+
+  desired_num_chunks = max(1, (file_size + chunk_size - 1) // chunk_size)
+
+  # Initial guesses for chunk boundary locations, uniformly spaced
+  # Chunks start on previous index, don't include last index
+  chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+  chunk_boundaries[-1] = file_size
+
+  mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+  for bi in range(1, len(chunk_boundaries) - 1):
+      initial_position = chunk_boundaries[bi]
+      file.seek(initial_position)  # Start at boundary guess
+      while True:
+          mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+          # If EOF, this boundary should be at the end of the file
+          if mini_chunk == b"":
+              chunk_boundaries[bi] = file_size
+              break
+
+          # Find the special token in the mini chunk
+          found_at = mini_chunk.find(split_special_token)
+          if found_at != -1:
+              chunk_boundaries[bi] = initial_position + found_at
+              break
+          initial_position += mini_chunk_size
+
+  # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+  return sorted(set(chunk_boundaries))
+
+
+def encode_file_to_bin(
+  tokenizer: Tokenizer,
+  input_path: Path,
+  output_path: Path,
+):
+  total_bytes = input_path.stat().st_size
+  output_path.parent.mkdir(parents=True, exist_ok=True)
+  
+  chunk_size = 2 ** 32
+  with input_path.open("rb") as f:
+    chunk_boundaries = find_chunk_boundaries(f, chunk_size, b"<|endoftext|>")
     
+  with input_path.open("rb") as f, output_path.open("wb") as out:
+    with tqdm(total=total_bytes, desc=f"Encoding", unit="B", unit_scale=True) as pbar:
+      for start, end in zip(chunk_boundaries[:-1], chunk_boundaries[1:]):
+        f.seek(start)
+        chunk_bytes = f.read(end - start)
+        chunk = chunk_bytes.decode("utf-8")
+        
+        token_ids = tokenizer.encode(chunk)
+        np_token_ids = np.array(token_ids, dtype=np.uint16)
+        np_token_ids.tofile(out)
+        
+        pbar.update(len(chunk_bytes))
+      
   
 def main():
   ROOT = Path(__file__).resolve().parents[1]
   train_input_path = ROOT / "data" / "TinyStoriesV2-GPT4-train.txt"
   valid_input_path = ROOT / "data" / "TinyStoriesV2-GPT4-valid.txt"
   
-  train_output_path = ROOT / "tokenizer_output" / "TinyStories-TokenIDs-train.bin"
-  valid_output_path = ROOT / "tokenizer_output" / "TinyStories-TokenIDs-valid.bin"
+  train_output_path = ROOT / "data" / "TinyStories-TokenIDs-train.bin"
+  valid_output_path = ROOT / "data" / "TinyStories-TokenIDs-valid.bin"
   
   vocab_path = ROOT / "tokenizer_output" / "tinystories_vocab.json"
   merges_path = ROOT / "tokenizer_output" / "tinystories_merge.txt"
@@ -159,22 +229,10 @@ def main():
   special_tokens = ["<|endoftext|>"]
   
   tokenizer = Tokenizer.from_files(vocab_path, merges_path, special_tokens)
-  
-  with valid_input_path.open("r", encoding="utf-8") as f:
-    text_valid = f.read()
-    
-  # This is assuming vocab_size < 2^16
-  # Which is fine since vocab size is 10000 for TinyStories and 32000 for OWT
-  token_ids = tokenizer.encode(text_valid)
-  np_token_ids = np.array(token_ids, dtype=np.uint16)
-  np_token_ids.tofile(valid_output_path)
-  
-  with train_input_path.open("r", encoding="utf-8") as f:
-    text_train = f.read()  
 
-  token_ids = tokenizer.encode(text_train)
-  np_token_ids = np.array(token_ids, dtype=np.uint16)
-  np_token_ids.tofile(train_output_path)
+  # This is assuming vocab_size < 2^16
+  encode_file_to_bin(tokenizer, valid_input_path, valid_output_path)
+  encode_file_to_bin(tokenizer, train_input_path, train_output_path)
     
   
 if __name__ == "__main__":
